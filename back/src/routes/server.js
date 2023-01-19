fa3bce26e423f8f10199b3073d84620d4add1bc8
fabianbm15 <cmelgarejobm@gmail.com>
2023-01-19 08:44:49 -0500

    19-01-2023

---
+++ back/src/routes/server.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const data = require("./../utils/data");
+PORT = 3001;
+
+http
+  .createServer((req, res) => {
+    if (!req.url.split("/")[3]) {
+      if (req.url.includes("rickandmorty/characters")) {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(data));
+      } else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Character not found");
+      }
+    } else if (req.url.includes("rickandmorty/character")) {
+      const idReq = Number(req.url.split("/")[3]);
+      const character = data.find((e) => e.id === idReq);
+      if (character) {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(character));
+      } else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Character not found");
+      }
+    } else {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("Character not found");
+    }
+  })
+  .listen(PORT, () => {
+    console.log("Server running at http://localhost:3001/");
+  });
