55d699ebc9be0e01d8753c1c34604e45b344c897
fabianbm15 <cmelgarejobm@gmail.com>
2023-03-30 18:05:09 -0500

    cors deploy back

--- api/app.js
+++ api/app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json({ limit: "50mb" }));
 
 //Seteamos headers para la respuesta que le enviamos al cliente
 app.use((req, res, next) => {
-   res.header("Access-Control-Allow-Origin", "http://localhost:3000, https://rickandmorty-fabian.netlify.app"); //Autorizo recibir solicitudes de este dominio
+   res.header("Access-Control-Allow-Origin", "https://rickandmorty-fabian.netlify.app"); //Autorizo recibir solicitudes de este dominio
    res.header("Access-Control-Allow-Credentials", true); //Autorizo recibir solicitudes que incluyan el encabezado con credenciales
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); //Autorizo recibir solicitudes con dichos hedears
    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE"); //Autorizo las solicitudes tipo GET, POST, OPTIONS, PUT y DELETE.
